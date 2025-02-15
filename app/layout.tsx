9ad3b9048fca757fe2e98647fd393be712105b22
ncolesummers <nsummers@uidaho.edu>
2025-02-15 14:56:46 -0800

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import './globals.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
+  title: 'Work Bot',
   description: 'Next.js chatbot template using the AI SDK.',
 };
 
